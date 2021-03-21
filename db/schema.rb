544e27898ba0cd93df66b499a05236514814a7a1
ko-horiuchi <k.h0520@docomo.ne.jp>
2021-03-21 04:52:20 +0000

    [add]genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_023308) do
+ActiveRecord::Schema.define(version: 2021_03_21_044847) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
