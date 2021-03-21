53175b8eadd3add560efde194990932f52e5d3be
ko-horiuchi <k.h0520@docomo.ne.jp>
2021-03-21 00:41:51 +0000

    [add]マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
+
+  get root to: "homes#top"
+  get "home/about" => "homes#about"
+  resources :users, only: [:show, :edit, :update]
+  resources :artists
+  
+  
+
 end
